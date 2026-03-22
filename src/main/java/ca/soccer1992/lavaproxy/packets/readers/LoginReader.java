447097af5b9387a2fd36852f937f27009b87bdf6
soccer1992 <soccer1992.is.a.dev@gmail.com>
2026-03-22 13:50:44 -0400

    register packet in LoginReader (Compression)

--- src/main/java/ca/soccer1992/lavaproxy/packets/readers/LoginReader.java
+++ src/main/java/ca/soccer1992/lavaproxy/packets/readers/LoginReader.java
@@ -3,7 +3,7 @@ package ca.soccer1992.lavaproxy.packets.readers;
 import ca.soccer1992.lavaproxy.MinecraftVersions;
 import ca.soccer1992.lavaproxy.packets.DefinitionPair;
 import ca.soccer1992.lavaproxy.packets.Packet;
-import ca.soccer1992.lavaproxy.packets.client.LoginKick;
+import ca.soccer1992.lavaproxy.packets.client.*;
 import ca.soccer1992.lavaproxy.packets.server.*;
 
 import java.util.List;
@@ -18,6 +18,9 @@ public class LoginReader extends Reader {
     public Map<Class<? extends Packet>, List<DefinitionPair>> clientDefinitions = Map.of(
             LoginKick.class, List.of(
                     new DefinitionPair(MinecraftVersions.MINECRAFT_1_8, 0x00)
+            ),
+            CompressionPacket.class, List.of(
+                    new DefinitionPair(MinecraftVersions.MINECRAFT_1_8, 0x03)
             )
     );
 
